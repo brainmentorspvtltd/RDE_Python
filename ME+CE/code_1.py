a10f9be5593007e5f421a9f3305dd2dfe38b647d
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-12-12 11:09:42 +0530

    Add files via upload

--- ME+CE/code_1.py
+++ ME+CE/code_1.py
@@ -3,6 +3,7 @@ b = 2
 c = a + b
 print(c)
 print("Sum is",c)
+print("Sum of a and b is c")
 print("Sum of", a, "and", b, "is", c)
 
 print("Sum is %d"%c)
@@ -10,7 +11,29 @@ print("Sum of %d and %d is %d"%(a,b,c))
 
 print("Sum is {}".format(c))
 print("Sum of {} and {} is {}".format(a,b,c))
-print("Sum of {1} and {0} is {2}".format(a,b,c))
+print("Sum of {} and {} is {}".format(a,b,c))
 
-#f-strings
+#f-strings (fast-strings)
 print(f"Sum of {a} and {b} is {c}")
+
+d = a - b
+e = a / b
+f = a * b
+
+#print("1. Add\n2. Sub\n3. Div")
+# Multi-line Print
+print(f"""
+1. Add is {c}
+2. Sub is {d}
+3. Div is {e}
+4. Mul is {f}
+""")
+
+
+
+
+
+
+
+
+
