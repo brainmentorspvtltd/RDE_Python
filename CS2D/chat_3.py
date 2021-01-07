551c434b839565920f69a2e8719306de07f25f0c
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2021-01-07 13:25:55 +0530

    Add files via upload

--- CS2D/chat_3.py
+++ CS2D/chat_3.py
@@ -21,7 +21,14 @@ while chat:
         path = r'C:\Users\acer\Music'
         os.chdir(path)
         songs = os.listdir()
+        '''
         song = random.choice(songs)
+        os.startfile(song)
+        '''
+        for i in range(len(songs)):
+            print(i+1, songs[i])
+        num = int(input("Enter song number you want to play : "))
+        song = songs[num-1]
         os.startfile(song)
     elif msg == "bye":
         print("Bye {}, Take care".format(name))
